beebf59607ceb8c2f1bdd2435f1928325c96d228
Andy Chen <amazingandyyy@gmail.com>
2019-01-17 21:42:31 -0800

    add nginx

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Feature!
+            Andy!
           </p>
           <a
             className="App-link"
